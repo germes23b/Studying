639ed33a5db1174a5d49b308c105f9c18e858715
germes23b <night.support.satnet@gmail.com>
2024-11-22 09:55:25 +0300

    Добавлена 22.11

---
+++ L7Work/calcCubeParams7.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcCubeParams7._2
+{
+    internal class Program
+    {
+        static void GetCubeVolSurf(double a, out double v, out double s)
+        {
+            v = a * a * a;
+            s = 6 * a * a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double v;
+            double s;
+            GetCubeVolSurf(a, out v, out s);
+            Console.WriteLine("Объем куба - {0:.00}, площадь его поверхности - {1:.00}", v, s);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ L7Work/compareTriangle7.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compareTriangle7._1
+{
+    internal class Program
+    {
+        static double GetSquare(double a, double b, double c)
+        {
+            double p = (double)(a + b + c) / 2;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины сторон первого треугольника: ");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            double c1 = Convert.ToDouble(Console.ReadLine());
+            double s1 = GetSquare(a1, b1, c1);
+
+            Console.WriteLine("Введите длины сторон второго треугольника: ");
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            double b2 = Convert.ToDouble(Console.ReadLine());
+            double c2 = Convert.ToDouble(Console.ReadLine());
+            double s2=GetSquare(a2, b2, c2);
+
+            if(s1>s2)
+                Console.WriteLine("Площадь первого треугольника больше");
+            else
+                if(s2>s1) Console.WriteLine("Площадь второго треугольника больше");
+            else
+                Console.WriteLine("Треугольники имеют равную площадь");
+            Console.ReadKey();
+        }
+    }
+}
