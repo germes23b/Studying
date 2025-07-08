6ae3d48070ed0d9ea569e0419ba8e14357e40465
germes23b <night.support.satnet@gmail.com>
2025-07-08 18:22:36 +0300

    L9

---
+++ L9Work/simpleCalc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Кошечка говорит \"мяу\", собачка говорит \"гав\", калькулятор говорит \"Приветствую. Я Калькулятор, смотритель Сети Убежищ.\"");
+                Console.Write("Введите первое число:");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите второе число:");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+            }
+            catch (exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
