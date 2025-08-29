587c913b921a4608525cf13ebe083e6a57825bba
germes23b <night.support.satnet@gmail.com>
2025-08-29 03:05:02 +0300

    L12

---
+++ L12Work/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L12Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты центра круга:");
+            double x0=Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите радиус круга:");
+            double r=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки:");
+            double x=Convert.ToDouble(Console.ReadLine());
+            double y=Convert.ToDouble(Console.ReadLine());
+
+            double cirLength = Round.GetCircleLength(r);
+            double cirSquare = Round.GetCircleSquare(r);
+            bool inTheCircle = Round.IsPointInCircle(x, y, x0, y0, r);
+            Console.WriteLine($"Введённый круг имеет длину окружности {cirLength} и площадь {cirSquare}.");
+            if(inTheCircle)
+                Console.WriteLine("Указанная точка лежит принадлежит кругу.");
+            else
+                Console.WriteLine("Указанная точка лежит вне круга.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ L12Work/Round.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L12Work
+{
+    static class Round
+    {
+        static public double GetCircleLength(double r)
+        {
+            return 2 * r * Math.PI;
+        }
+
+        static public double GetCircleSquare(double r)
+        {
+            return r * r * Math.PI;
+        }
+
+        static public bool IsPointInCircle(double x, double y, double r, double x0, double y0)
+        {
+            if ((x - x0) * (x - x0) + (y - y0) * (y - y0) <= r * r)
+                return true;
+            else return false;
+        }
+    }
+}
