ceb0e48594dcada6313ef3e4e76be43e0d49cf8a
germes23b <night.support.satnet@gmail.com>
2025-08-29 03:05:02 +0300

    L10

---
+++ L10Work/L10Work/Angle.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L10Work
+{
+    class Angle
+    {
+        int gradus;
+        int minute;
+        int second;
+        bool direction = true;     //  Введём направление, против часовой - true(положительный угол), по - false(отрицательный)
+
+        public int Gradus       //  Учитывая запись градусы°минуты'секунды" - знак оставляем только у градусов как направление угла.
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value < 0)      
+                {
+                    value = Math.Abs(value);        //  Берём градусы по модулю, чтобы прибавлять минуты и секунды. Должно сработать.
+                    direction = false;      //  Укажем направление
+                }
+                if (value > 360) gradus = value % 360;      //  Если введено больше полного оборота - интересует только остаток.
+                else gradus = value;
+            }
+        }
+
+        public int Minute
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                Gradus += value / 60;
+                minute = value % 60;
+            }
+        }
+
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                Minute += value / 60;
+                second = value % 60;
+            }
+        }
+
+        public Angle(int gradus, int minute, int second)
+        {
+            this.Gradus = gradus;
+            this.Minute = minute;
+            this.Second = second;
+        }
+
+        public double ToRadians()
+        {
+            if(direction) return (gradus + minute / 60 + second / 3600) * Math.PI / 180;        //  Положительный угол
+            else return -((gradus + minute / 60 + second / 3600) * Math.PI / 180);      //  Отрицательный угол. Возможно, стоит вернуть direction=true после.
+        }
+    }
+
+
+
+}
---
+++ L10Work/L10Work/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L10Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение градусов угла:");
+            int curGradus = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение минут угла:");
+            int curMinute = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение секунд угла:");
+            int curSecond = Convert.ToInt32(Console.ReadLine());
+
+            Angle angle= new Angle(curGradus,curMinute, curSecond);
+
+            double angleInRadians=angle.ToRadians();
+            Console.WriteLine("Значение угла в радианах: {0}",angleInRadians);
+            Console.ReadKey();
+        }
+    }
+}
