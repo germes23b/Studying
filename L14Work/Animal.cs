a9596b5b40bca603d067401bf5dcd20a5f774ada
germes23b <night.support.satnet@gmail.com>
2025-09-11 17:54:02 +0300

    L14Work

---
+++ L14Work/Animal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L14Work
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name+" говорит: ");
+            Say();
+        }
+    }
+    class Cat : Animal
+    {
+        private string name;
+        public override string Name 
+        { 
+            get => name; 
+            set => name=value; 
+        }
+        public Cat(string name) : base(name) { }
+
+        public override void Say()
+        {
+            Console.WriteLine("Мяу!");
+        }
+    }
+    class Dog : Animal
+    {
+        private string name;
+
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Dog(string name) : base(name) { }
+
+        public override void Say()
+        {
+            Console.WriteLine("Гав!");
+        }
+    }
+}
---
+++ L14Work/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L14Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как зовут котика?");
+            string catName=Console.ReadLine();
+            Console.WriteLine("Как зовут собачку?");
+            string dogName = Console.ReadLine();
+            Cat cat = new Cat(catName);
+            Dog dog = new Dog(dogName);
+            cat.ShowInfo();
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
