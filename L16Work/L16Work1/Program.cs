3ea67a5ca6ed6fd036ea626c3dfef6d903277478
germes23b <night.support.satnet@gmail.com>
2025-09-25 01:11:11 +0300

    L16Work

---
+++ L16Work/L16Work1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace L16Work1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            Products[] products = new Products[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите код товара:");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите название товара:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введите цену товара:");
+                double price = Convert.ToDouble(Console.ReadLine());
+
+                products[i]= new Products() { ID=id, Name=name, Price=price};
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            string jsonString=JsonSerializer.Serialize(products, options);
+
+            using (StreamWriter sw = new StreamWriter("../../../Products.json"))
+            { 
+                sw.WriteLine(jsonString);
+            }
+        }
+    }
+}
---
+++ L16Work/L16Work2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace L16Work2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = string.Empty;
+            using (StreamReader sr = new StreamReader("../../../Products.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+
+            Products[] products = JsonSerializer.Deserialize<Products[]>(jsonString);
+
+            Products maxPrice = products[0];
+            foreach (Products p in products)
+            {
+                if (p.Price > maxPrice.Price)
+                {
+                    maxPrice = p;
+                }
+            }
+            Console.WriteLine($"Самый дорогой товар - {maxPrice.Name}");
+            Console.ReadKey();
+        }
+    }
+}
