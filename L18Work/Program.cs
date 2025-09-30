1b5dbe84afbd740ad11d652951b2c98dbf2c465b
germes23b <night.support.satnet@gmail.com>
2025-09-30 17:35:44 +0300

    L18Work

---
+++ L18Work/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L18Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string str = Console.ReadLine();
+            Console.WriteLine(Check(str));
+            Console.ReadKey();
+        }
+        static bool Check(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> braces = new Dictionary<char, char>
+            {
+                { '(',')'},
+                { '{','}'},
+                { '[',']'},
+            };
+            foreach (char c in str)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                    stack.Push(braces[c]);
+                if (c == ')' || c == '}' || c == ']')
+                {
+                    if (stack.Count == 0 || stack.Pop() != c)
+                        return false;
+                }
+            }
+            if (stack.Count == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
