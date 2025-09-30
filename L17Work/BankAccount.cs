baa864eae6254d296c16971ba817db06a4180802
germes23b <night.support.satnet@gmail.com>
2025-09-30 14:59:39 +0300

    L17Work

---
+++ L17Work/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L17Work
+{
+    class BankAccount<T>
+    {
+        private T AccNumber { get; set; }
+        private double Balance { get; set; }
+        private string Fio { get; set; }
+
+        public void GetInfo()
+        {   
+            Console.WriteLine($"Номер счёта: {AccNumber} Баланс: {Balance} ФИО: {Fio}");
+        }
+
+        public void SetInfo()
+        {
+            Console.WriteLine("Введите номер счёта:");
+            AccNumber=(T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите сумму баланса:");
+            Balance=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО:");
+            Fio= Console.ReadLine();
+        }
+    }
+}
---
+++ L17Work/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L17Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> bankAcc1 = new BankAccount<int>();
+            BankAccount<string> bankAcc2 = new BankAccount<string>();
+            Console.WriteLine("Первый счёт(числовой формат)");
+            bankAcc1.SetInfo();
+            Console.WriteLine("Второй счёт(текстовый формат)");
+            bankAcc2.SetInfo();
+            bankAcc1.GetInfo();
+            bankAcc2.GetInfo();
+            Console.ReadKey();
+        }
+    }
+}
