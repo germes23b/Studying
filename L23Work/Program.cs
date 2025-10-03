653d9f264e2d76721c6e17bc7be849c17eed9d4f
germes23b <night.support.satnet@gmail.com>
2025-10-03 18:07:43 +0300

    L23Work

---
+++ L23Work/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L23Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            FactorialAsync(n);
+
+            Console.ReadKey();
+        }
+        static void Factorial(int n)
+        {
+            int res = 1;
+            for (int i = 1; i < n; i++)
+            {
+                res *= i;
+            }
+            Console.WriteLine($"Результат: {res}");
+        }
+        static async void FactorialAsync(int n)
+        {
+            await Task.Run(() => Factorial(n));
+        }
+    }
+}
